c7bbc093ae14d9b9ac78539da4af5fa95109e84a
yaric <yaric@yared>
2023-05-12 08:36:12 -0700

    I commite my project to my local git

---
+++ src/tespro/Test1.java
@@ -0,0 +1,5 @@
+package tespro;
+
+public class Test1 {
+
+}
---
+++ src/tespro/test2.java
@@ -0,0 +1,11 @@
+package tespro;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("this is my first git");
+
+	}
+
+}
